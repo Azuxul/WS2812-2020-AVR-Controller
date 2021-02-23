351ed045524c7dabf6bca93617fc38dd3408236d
Lancelot <lancelot@azuxul.fr>
2021-02-23 17:42:03 +0100

    Start to use PWM timer instead of ASM code

--- src/main.c
+++ src/main.c
@@ -2,15 +2,41 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define LED_NUMBER 2
+#define LED_NUMBER 1
 
 uint8_t pinPort = _SFR_IO_ADDR(PORTD);
 uint8_t pinBit = 3;
 
-uint8_t colors[LED_NUMBER][3];
+volatile uint8_t colors[LED_NUMBER][3];
+
+volatile uint8_t led = 0;
+volatile uint8_t ledByte = 0;
+volatile uint8_t bit = 0;
+
+ISR(TIMER1_OVF_vect) {
+
+  ICR1 = 0x00FF;
+
+  if((colors[led][ledByte] >> bit++) & 1) {
+    OCR1A = 100;
+  } else {
+    OCR1A = 200;
+  }
+
+  if(bit > 7) {
+    bit = 0;
+    if(++led > 2) {
+      ledByte = 0;
+      if(++led >= LED_NUMBER) {
+        led = 0;
+        ICR1 = 0xFFFF;
+      }
+    }
+  }
+} 
 
 // LSB of each color byte shoud be 0 (otherwise there is timing issue)
-void updateLed(uint8_t port, uint8_t pinBit, volatile uint8_t colorsArray[][3], uint8_t size)
+void updateLedASM(uint8_t port, uint8_t pinBit, volatile uint8_t colorsArray[][3], uint8_t size)
 {
 
   uint8_t counter = 0;
@@ -48,6 +74,7 @@ void updateLed(uint8_t port, uint8_t pinBit, volatile uint8_t colorsArray[][3], 
     "tx_led_bit_end%=:"
       "cbi %2, %3 \n\t"
       "breq tx_led_data%= \n\t"
+      "nop \n\t"
       "brne tx_led_bit%= \n"
     "tx_led_data_end%=:\n\t"
       "clr %5"
@@ -64,18 +91,39 @@ void updateLed(uint8_t port, uint8_t pinBit, volatile uint8_t colorsArray[][3], 
 int main(void)
 {
 
-  DDRD = 1 << 3;
+  
+  colors[0][0] = 0xFF;
+  colors[0][1] = 0b10000100;
+  colors[0][2] = 0b10010000;
 
-  colors[0][0] = 0b11111110;
-  colors[0][1] = 0b11111110;
-  colors[0][2] = 0b11111110;
-  colors[1][0] = 0b11111110;
-  colors[1][1] = 0b11111110;
-  colors[1][2] = 0b11111110;
+  sei();
+
+  TCNT1 = 0; //Initialize Timer 1 counter
+  DDRB |= (1 << DDB1); //PB1 is an output
+    DDRB |= (1 << DDB1); // PB1 is set as an output 
+
+    ICR1 = 0x0FFF; // set PWM counter TOP
+
+    OCR1A = 0x0000; // set 0% PWM dutty cycle
+
+    TCCR1A |= (1 << COM1A1)|(1 << COM1B1); // set non inverting mode
+
+    TCCR1A |= (1 << WGM11);
+    TCCR1B |= (1 << WGM12)|(1 << WGM13);
+    // set Fast PWM mode using ICR1 as TOP
+    
+    TCCR1B |= (1 << CS10); // start timer with no prescaler
+    TIMSK1 |= (1 << TOIE1); // set intreup on timer overflow  
+
+  while(1){
+
+  }
+
+
 
   for (;;)
   {
-    updateLed(pinPort, pinBit, colors, LED_NUMBER);
+    //updateLed(pinPort, pinBit, colors, LED_NUMBER);
 
     _delay_ms(50);
   }
